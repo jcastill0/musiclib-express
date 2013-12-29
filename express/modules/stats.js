f685409c161c6c7e2bcb59e6055971fb2756b5f6
Julio Castillo <julio.castillo.2010@gmail.com>
2013-12-28 21:44:12 -0800

    fixed bug in stats

--- express/modules/stats.js
+++ express/modules/stats.js
@@ -3,19 +3,11 @@ var config = require('./config');
 module.exports = Stats;
 
 function Stats() {
-  this.totalUsers = null;
-  this.totalSongs = null;
-  this.totalPlaylists = null;
-  this.totalArtists = null;
 };
 
 Stats.getAll= function (cb) {
     if (config.debug)
 	console.log("Stats.getAll");
-    var totalUsers = null;
-    var totalSongs = null;
-    var totalPlaylists = null;
-    var totalArtists = null;
     config.getConnPool().getConnection (function(error, connection) {
       if (error) {
 	  console.error("Connection Pool Error: " + error.message);
@@ -30,12 +22,7 @@ Stats.getAll= function (cb) {
 	    cb(error);
 	}
 	if (row.length == 1) {
-	    this.totalUsers = row[0];
-	    this.totalSongs = row[1];
-	    this.totalPlaylists = row[2];
-	    this.totalArtists = row[3];
-console.log("U:"+this.totalUsers+" S:"+this.totalSongs+" P:"+this.totalPlaylists+" A:"+this.totalArtists);
-	    cb (null, {"totalUsers":this.totalUsers, "totalSongs":this.totalSongs, "totalPlaylists":this.totalPlaylists, "totalArtists":this.totalArtists});
+	    cb (null, row[0]);
 	} else {
 	    cb("Record not found");
 	}
